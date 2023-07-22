d4155654a5c4b916e15febb36b6b04b23b8d3b6e
Md Imran <imranuddinctg@gmail.com>
2023-07-22 22:04:10 +0600

    Start project in Django and create user auth app

---
+++ Ami_coding_pari_na/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ami_coding_pari_na project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ami_coding_pari_na.settings')
+
+application = get_asgi_application()
