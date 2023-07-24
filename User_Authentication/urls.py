46467e40e71d75162a7d66f7843ee9e64df3fdd9
Md Imran <imranuddinctg@gmail.com>
2023-07-24 22:36:03 +0600

    change in urls.py

--- User_Authentication/urls.py
+++ User_Authentication/urls.py
@@ -6,7 +6,6 @@ from .views.signup_views import SignupPage
 from .views.api_views import get_all_input_values
 urlpatterns = [
     path('', SignupPage, name='signup'),
-    path('register/', SignupPage, name='signup'),
     path('login/', LoginPage, name='login'),
     path('khoj_search/', khoj_search, name='khoj_search'),
     path('logout/',LogoutPage,name='logout'),
